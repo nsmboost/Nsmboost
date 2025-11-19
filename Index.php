73422aaf09d01a97865bb6d1edc11ce0efef1403
nsmboost <nsmboost@gmail.com>
2025-11-19 03:52:03 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,22 @@
+<?php
+// Start the session
+session_start();
 
+// Include the configuration file
+require_once 'includes/config.php';
+
+// Include the database file
+require_once 'includes/db.php';
+
+// Define the page title
+$page_title = 'Nsmboost - Social Media Marketing Platform';
+
+// Include the header file
+include 'pages/header.php';
+
+// Include the content file
+include 'pages/index-content.php';
+
+// Include the footer file
+include 'pages/footer.php';
+?>
