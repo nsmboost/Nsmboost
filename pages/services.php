67fbb4b93e6cc372239888413872812e1b84ce51
nsmboost <nsmboost@gmail.com>
2025-11-19 04:07:52 +0100

    Create services.php

---
+++ pages/services.php
@@ -0,0 +1,16 @@
+<?php
+$page_title = 'Our Services';
+include 'header.php';
+?>
+
+<section>
+    <h1>Our Services</h1>
+    <ul>
+        <li>Instagram Followers</li>
+        <li>Facebook Likes</li>
+        <li>YouTube Views</li>
+        <li>Tiktok Followers</li>
+    </ul>
+</section>
+
+<?php include 'footer.php'; ?>
