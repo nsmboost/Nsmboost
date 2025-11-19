d6774ad809e4b6bb1a6d71c0abca3b138ede8ece
nsmboost <nsmboost@gmail.com>
2025-11-19 04:06:32 +0100

    Create about.php

---
+++ pages/about.php
@@ -0,0 +1,11 @@
+<?php
+$page_title = 'About Us';
+include 'header.php';
+?>
+
+<section>
+    <h1>About Us</h1>
+    <p>Nsmboost is a social media marketing platform that offers affordable services to boost online presence.</p>
+</section>
+
+<?php include 'footer.php'; ?>
