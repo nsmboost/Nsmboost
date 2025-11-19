bfd369484ac76fca4048e34c54832fe60d607c3b
nsmboost <nsmboost@gmail.com>
2025-11-19 05:00:10 +0100

    Create sitemap.php

---
+++ pages/sitemap.php
@@ -0,0 +1,19 @@
+<?php
+$page_title = 'Sitemap';
+include 'header.php';
+?>
+
+<section>
+    <h1>Sitemap</h1>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="faq.php">FAQs</a></li>
+        <li><a href="terms.php">Terms and Conditions</a></li>
+        <li><a href="privacy.php">Privacy Policy</a></li>
+    </ul>
+</section>
+
+<?php include 'footer.php'; ?>
