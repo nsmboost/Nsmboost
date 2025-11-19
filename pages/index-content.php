3cc58465153e27e38c00a0bd9c6d122c92c61f93
nsmboost <nsmboost@gmail.com>
2025-11-19 04:02:55 +0100

    Create index-content.php

---
+++ pages/index-content.php
@@ -0,0 +1,5 @@
+<section>
+    <h1>Welcome to Nsmboost</h1>
+    <p>Nsmboost is a social media marketing platform that offers affordable services to boost online presence.</p>
+    <button>Get Started</button>
+</section>
