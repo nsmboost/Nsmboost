b8e04399f30ab7549332ce3201a68d2bf9fe0449
nsmboost <nsmboost@gmail.com>
2025-11-19 04:48:16 +0100

    Create terms.php

---
+++ pages/terms.php
@@ -0,0 +1,16 @@
+<?php
+$page_title = 'Terms and Conditions';
+include 'header.php';
+?>
+
+<section>
+    <h1>Terms and Conditions</h1>
+    <p>These terms and conditions outline the rules and regulations for the use of our website.</p>
+    <p>By accessing this website, we assume you accept these terms and conditions. Do not continue to use Nsmboost if you do not agree to take all of the terms and conditions stated on this page.</p>
+    <h2>License</h2>
+    <p>Unless otherwise stated, Nsmboost and/or its licensors own the intellectual property rights for all material on Nsmboost. All intellectual property rights are reserved.</p>
+    <h2>Disclaimer</h2>
+    <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website.</p>
+</section>
+
+<?php include 'footer.php'; ?>
