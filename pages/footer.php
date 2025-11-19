9a118b6a7780567b84fff12a085687ddedd4703f
nsmboost <nsmboost@gmail.com>
2025-11-19 04:04:14 +0100

    Create footer.php

---
+++ pages/footer.php
@@ -0,0 +1,7 @@
+    </main>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> Nsmboost. All rights reserved.</p>
+    </footer>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
